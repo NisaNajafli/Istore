78fce2fcc660152aec0233c7793e046583a02954
Nisa Najafli <ncfliniss@gmail.com>
2026-01-08 17:38:13 +0400

    new

---
+++ assests/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hero = document.querySelector(".appleHeroSwiper");
+  if (!hero) return;
+
+  new Swiper(".appleHeroSwiper", {
+    loop: true,
+    speed: 900,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false
+    // },
+    pagination: {
+      el: ".apple-hero__pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".apple-hero__next",
+      prevEl: ".apple-hero__prev"
+    },
+    grabCursor: true
+  });
+});
