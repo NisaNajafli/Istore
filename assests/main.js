ae3dd8cad88eac35a5ec69a437c4c3b9b014b072
Nisa Najafli <ncfliniss@gmail.com>
2026-01-10 01:58:05 +0400

    new

--- assests/main.js
+++ assests/main.js
@@ -20,3 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
     grabCursor: true
   });
 });
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 200) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
